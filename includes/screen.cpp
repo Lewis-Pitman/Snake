e007cb2e418151626dbcb3995c9eb221e05e4361
Lewis-Pitman <lpitman08@outlook.com>
2024-09-09 19:01:17 +0100

    Setting up the project
    Modifying screen.cpp to better suit the project

---
+++ includes/screen.cpp
@@ -0,0 +1,73 @@
+//this file contains all the logic needed to initialise and render the grid
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+
+char** spaces;
+
+class Screen
+{
+    private:
+    int width {};
+    int height {};
+    int spacing {};
+    bool turn {false}; //false = X, true = O
+
+    void DrawLine(){
+            std::cout << std::setw(spacing + 1);
+            for(int i = 0; i < width; i++){
+                for(int j = 0; j < (spacing * 2); j++)
+                std::cout << "-";
+            }
+            std::cout << "-";
+    }
+
+    void DrawScreen(){//contains the logic for drawing screen
+        std::cout << "\n";
+        DrawLine(); //dividing line under the number line
+        std::cout << "\n";
+
+        for(int i = 0; i < height; i++){
+            std::cout << std::setw(spacing + 1); //move grid further away from left side
+            for(int j = 0; j < width; j++){
+                std::cout << "|" << std::setw(spacing) << spaces[i][j] << std::setw(spacing); 
+            }
+        
+        std::cout << "|";
+
+        std::cout << "\n";
+        DrawLine(); //dividing line
+        std::cout << "\n";
+
+    }
+    }
+
+    public:
+    
+    Screen(int _width, int _height, int _spacing){
+        width = _width;
+        height = _height;
+        spacing = _spacing;
+
+        spaces = new char*[_height];
+        for(int i = 0; i < _height; i++){
+            spaces[i] = new char[_width];
+        } //2D array initialisation | "spaces" is a pointer to an array of pointers. Each pointer in this pointer array points to the memory address of another pointer.
+
+        for(int i = 0; i < _height; i++){
+            for(int j = 0; j < _width; j++){
+                spaces[i][j] = ' '; //Initialise all elements in the 2D array to " " so that the grid displays properly
+            }
+        }
+
+    }//constructor
+
+    ~Screen(){
+    }//deconstructor
+
+    void Render(){
+        DrawScreen();
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include "includes\screen.cpp"
+
+//screen
+int width;
+int height;
+int padding;
+
+//----------------------------------------
+//general functions
+
+void Initialise(){
+    std::cout << "Enter width of play area (recommended: 10): ";
+    std::cin >> width;
+    std::cout << "\nEnter height of play area (recommended: 10): ";
+    std::cin >> height;
+    std::cout << "\nEnter padding (distance between tiles, recommended: 2) of play area: ";
+    std::cin >> padding;
+}
+
+void Hang(){//this is so the program doesn't exit straight away, the user has to enter -1 in order to close the program
+    int response {};
+    while(response != -1){
+        std::cout << "\nEnter -1 to exit the program: ";
+        std::cin >> response;
+    }
+}
 
 int main(){
+    Initialise();
+    Screen screen(width, height, padding);
+    screen.Render();
+    Hang();
     return 0;
 }
